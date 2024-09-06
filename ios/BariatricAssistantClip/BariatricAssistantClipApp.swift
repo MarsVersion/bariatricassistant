2d0907497057582ac607ad6be536ef018aecf05d
mv <deukor@yahoo.com>
2024-09-06 18:35:20 +0200

    Update files from Expo project

---
+++ ios/BariatricAssistantClip/BariatricAssistantClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BariatricAssistantClipApp.swift
+//  BariatricAssistantClip
+//
+//  Created by DISKURS Berlin on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct BariatricAssistantClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
